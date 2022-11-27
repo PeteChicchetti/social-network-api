e89ae7390e3754a58da813b3896ac54563147cb3
PeteChicchetti <pj0524@gmail.com>
2022-11-27 11:08:12 -0500

    added User Model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// User model
+const userSchema =  new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Virtual property to show number of friends
+userSchema.virtual('friendCount').get(function () {
+    return this.comments.length;
+  });
+
+const User = model('User', userSchema);
+
+module.exports = User;
