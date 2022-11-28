5212db2e0b54a0b5b0ba097d75ed06b9935ce0c3
PeteChicchetti <pj0524@gmail.com>
2022-11-28 17:06:08 -0500

    added GET all thoughts and GET single thoughts routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,28 @@
-const { Thought, User } = require('mongoose');
+const { Thought, User } = require('../models/');
 
-
+const thoughtController = {
+    // Get all thoughts by .find()
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) =>
+                res.status(200).json(thoughts))
+            .catch((err) => 
+                res.status(500).json(err));   
+    },
+    // Get singleThought by .findOne()
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        // Remove version from the return
+            .select('-__v')
+            .then((thoughts) => 
+                !thought
+                    ? res.status(404).json({ 
+                        message: 'There is no thought that matches the requested ID' 
+                        })
+                    : res.status(200).json(thoughts)
+            )
+            .catch((err) => res.status(500).json(err));
+    }
+}
 
 module.exports = thoughtController;
