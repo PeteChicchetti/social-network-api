d146d98db9e899a69fc4dc6424d63dee67526a5b
PeteChicchetti <pj0524@gmail.com>
2022-11-28 15:03:33 -0500

    refactor routes to read easier

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,12 +10,15 @@ const {
 } = require('../../controllers/user-Controller');
 
 // route to /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/')
+    .get(getUsers).post(createUser);
 
 // route to /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+    .get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // route to /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId')
+    .post(addFriend).delete(removeFriend);
 
 module.exports = router;
