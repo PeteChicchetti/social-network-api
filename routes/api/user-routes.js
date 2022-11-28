1c5b0917d9329b6014a6b904c4ccca00628058ae
PeteChicchetti <pj0524@gmail.com>
2022-11-28 15:01:05 -0500

    added routes for users and friends

---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-Controller');
+
+// route to /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// route to /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// route to /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
