4cee6f19f3b358032e1bba7e7e2ae65ece9cf9ff
PeteChicchetti <pj0524@gmail.com>
2022-11-28 19:00:07 -0500

    corrected typo from thoughts to thought to return value

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -14,12 +14,12 @@ const thoughtController = {
         Thought.findOne({ _id: req.params.thoughtId })
         // Remove version from the return
             .select('-__v')
-            .then((thoughts) => 
+            .then((thought) => 
                 !thought
                     ? res.status(404).json({ 
                         message: 'There is no thought that matches the requested ID' 
                         })
-                    : res.status(200).json(thoughts)
+                    : res.status(200).json(thought)
             )
             .catch((err) => res.status(500).json(err));
     },
